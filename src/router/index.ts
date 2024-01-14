6d1fcffd2be5359c0a0e156ac33b1dc2bfab79dc
Anton Novoselov <lazygnom@gmail.com>
2024-01-14 18:04:20 +0300

    add routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,53 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MainView from '../views/MainView.vue'
+import CurrentWeatherView from '@/views/CurrentWeatherView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'main',
-      component: MainView
+      name: 'current',
+      component: CurrentWeatherView
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
+      path: '/forecast',
+      name: 'forecast',
+      component: () => import('@/views/ForecastView.vue')
+    },
+    {
+      path: '/future',
+      name: 'future',
+      component: () => import('@/views/FutureView.vue')
+    },
+    {
+      path: '/history',
+      name: 'history',
+      component: () => import('@/views/HistoryView.vue')
+    },
+    {
+      path: '/marine',
+      name: 'marine',
+      component: () => import('@/views/MarineView.vue')
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: () => import('@/views/SearchView.vue')
+    },
+    {
+      path: '/ip',
+      name: 'ip',
+      component: () => import('@/views/IpView.vue')
+    },
+    {
+      path: '/timezone',
+      name: 'timezone',
+      component: () => import('@/views/TimezoneView.vue')
+    },
+    {
+      path: '/astronomy',
+      name: 'astronomy',
+      component: () => import('@/views/AstronomyView.vue')
     }
   ]
 })
